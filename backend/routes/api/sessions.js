76087caaba2df8483f84015aceaf30575819cd18
Anurag Reddy <122401303+anureddy11@users.noreply.github.com>
2024-03-11 15:43:32 -0700

    Added current user-session details

--- backend/routes/api/sessions.js
+++ backend/routes/api/sessions.js
@@ -53,10 +53,36 @@ router.post('/', async (req,res,next) => {
 
 })
 
+
+// logout
 router.delete("/", async(req,res,next) => {
     res.clearCookie('token')
     return res.json({ message: "success"})
 
+
+})
+
+
+//Restore session user
+
+router.get("/", async(req,res,next) => {
+
+    const {user} = req
+    if (user){
+
+        const safeUser = {
+            id: user.id,
+            email: user.email,
+            username: user.username
+        }
+
+        return res.json({
+            user: safeUser
+        })
+
+    } else  return res.json({
+        user: null
+    })
 
 })
 
