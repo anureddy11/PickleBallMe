5c93673b42d3b3db5ae7f50e1d836fc6c6fb6286
Anurag Reddy <122401303+anureddy11@users.noreply.github.com>
2024-03-13 20:19:20 -0700

    edited correction for options in the seeder file

--- backend/db/seeders/20240309215331-demo-user.js
+++ backend/db/seeders/20240309215331-demo-user.js
@@ -39,7 +39,7 @@ module.exports = {
   async down (queryInterface, Sequelize) {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
+    return queryInterface.bulkDelete('Users', {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
     }, options);
   }
