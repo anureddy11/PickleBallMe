c66dd77a14eb55a1f52b489f5606fd62f337ae5e
Anurag Reddy <122401303+anureddy11@users.noreply.github.com>
2024-03-25 22:42:00 -0700

    added endpoint edit-group

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -18,4 +18,59 @@ router.get('/', async(req,res) => {
     return res.json({groups})
 })
 
+//Edit a group
+router.put('/:id', async(req,res,next) => {
+    try {
+        // Your code here
+        let updates = req.body
+        let {id} = req.params
+        let {organizer_id,name,about,type,private,city,state} = updates
+
+        const groupToUpdate = await Group.findOne({
+            where:{
+                id:id
+            }
+        })
+
+
+
+        if (groupToUpdate) {
+            // Update only the fields that exist in the updates object
+            for (const key in updates) {
+                if (updates.hasOwnProperty(key)) {
+                    groupToUpdate[key] = updates[key];
+                }
+            }
+        
+            await groupToUpdate.save()
+
+            res.json({
+                data: id,
+                status: "success",
+                message: `Successfully updated group`,
+            });
+        }
+
+        else{
+
+            next({
+                status: "not-found",
+                message: `Could not update group ${id}`,
+                details: 'group not found'
+            });
+
+        }
+
+
+
+    } catch(err) {
+        next({
+            status: "error",
+            message: `Could not update group ${id}`,
+            details: err.errors ? err.errors.map(item => item.message).join(', ') : err.message
+        });
+    }
+
+})
+
 module.exports = router;
