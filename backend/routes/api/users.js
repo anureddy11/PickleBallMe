1afb156988e6421e06034324fafe6acd35987b0e
Anurag Reddy <122401303+anureddy11@users.noreply.github.com>
2024-04-06 13:19:19 -0700

    further edits to signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -25,10 +25,14 @@ const validateSignup = [
     .not()
     .isEmail()
     .withMessage('Username cannot be an email.'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage('Password must be 6 characters or more.'),
+  check('firstName')
+    .exists({checkFalsy:true})
+    .withMessage("First Name is required"),
+   check('lastName')
+    .exists({checkFalsy:true})
+    .withMessage("Last Name is required"),
+
+
   handleValidationErrors
 
 ]
