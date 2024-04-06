bf5335288e26aa197ba915b708f745b1f517059b
Anurag Reddy <122401303+anureddy11@users.noreply.github.com>
2024-04-06 15:41:36 -0700

    re-did the stack removal

--- backend/app.js
+++ backend/app.js
@@ -83,12 +83,22 @@ app.use(routes); // Connect all the routes
 app.use((err, _req, res, _next) => {
     res.status(err.status || 500);
     console.error(err);
-    res.json({
-      title: err.title || 'Server Error',
-      message: err.message,
-      errors: err.errors,
-      stack: isProduction ? null : err.stack
-    });
-  });
+    if(isProduction){
+      res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+      })}
+      else {
+        res.json({
+          title: err.title || 'Server Error',
+          message: err.message,
+          errors: err.errors,
+          stack: isProduction ? null : err.stack
+        });
+      }
+    }
+
+  );
 
 module.exports = app;
